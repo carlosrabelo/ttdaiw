7d98131fb5bcf676e40c5a1c24e817191ab5e97f
Carlos Rabelo <developer@carlosrabelo.com.br>
2020-04-15 08:02:14 -0400

    2020-04-15 08:02

---
+++ install.bat
@@ -0,0 +1,17 @@
+@echo off
+
+scoop install 7zip
+scoop install composer
+scoop install curl
+scoop install ffmpeg
+scoop install git
+scoop install go
+scoop install nmap
+scoop install php
+scoop install putty
+scoop install python
+scoop install tar
+scoop install unzip
+scoop install wget
+scoop install youtube-dl
+scoop install zip
