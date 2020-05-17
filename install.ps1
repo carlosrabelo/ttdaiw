66898c591c559376f65e420c27c2994761097263
Carlos Rabelo <developer@carlosrabelo.com.br>
2020-05-17 15:38:26 -0400

    2020-05-17 15:38

---
+++ install.ps1
@@ -0,0 +1,49 @@
+<#
+
+Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+
+scoop bucket add extras
+scoop bucket add java
+
+#>
+
+scoop install 7zip
+scoop install arduino
+scoop install audacity
+scoop install cacert
+scoop install composer
+scoop install curl
+scoop install dark
+scoop install ffmpeg
+scoop install fritzing
+scoop install git
+scoop install go
+scoop install godot
+scoop install gzip
+scoop install hugo-extended
+scoop install imagemagick
+scoop install innounp
+scoop install julia
+scoop install kdenlive
+scoop install ldap-admin
+scoop install lessmsi
+scoop install lmms
+scoop install mobaxterm
+scoop install openssl
+scoop install php
+scoop install picotorrent
+scoop install putty
+scoop install python
+scoop install rufus
+scoop install vlc
+scoop install xming
+scoop install youtube-dl
+
+scoop install adb
+scoop install scrcpy
+
+scoop install find-java
+scoop install oraclejdk
+scoop install dart
+scoop install flutter
+scoop install android-sdk
