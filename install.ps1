dadbc97d360906ac7589cc6cb76fc50ae5ae5875
Carlos Rabelo <developer@carlosrabelo.com.br>
2020-06-01 21:28:53 -0400

    2020-06-01 21:29

--- install.ps1
+++ install.ps1
@@ -1,49 +1,39 @@
 <#
 
-Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
-
-scoop bucket add extras
-scoop bucket add java
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 #>
 
-scoop install 7zip
-scoop install arduino
-scoop install audacity
-scoop install cacert
-scoop install composer
-scoop install curl
-scoop install dark
-scoop install ffmpeg
-scoop install fritzing
-scoop install git
-scoop install go
-scoop install godot
-scoop install gzip
-scoop install hugo-extended
-scoop install imagemagick
-scoop install innounp
-scoop install julia
-scoop install kdenlive
-scoop install ldap-admin
-scoop install lessmsi
-scoop install lmms
-scoop install mobaxterm
-scoop install openssl
-scoop install php
-scoop install picotorrent
-scoop install putty
-scoop install python
-scoop install rufus
-scoop install vlc
-scoop install xming
-scoop install youtube-dl
+choco install -y 7zip
+choco install -y audacity
+choco install -y curl
+choco install -y ffmpeg
+choco install -y git
+choco install -y gzip
+choco install -y hugo-extended
+choco install -y imagemagick
+choco install -y ldapadmin
+choco install -y lmms
+choco install -y putty
+choco install -y rufus
+choco install -y scrcpy
+choco install -y vlc
+choco install -y youtube-dl
 
-scoop install adb
-scoop install scrcpy
+choco install -y obs-studio
+choco install -y obs-virtualcam
+choco install -y obs-ndi
 
-scoop install find-java
-scoop install oraclejdk
-scoop install dart
-scoop install flutter
-scoop install android-sdk
+choco install -y vscode
+
+choco install -y arduino
+choco install -y fritzing
+
+choco install -y godot
+
+choco install -y golang
+
+choco install -y python
+
+choco install -y php
+choco install -y composer
